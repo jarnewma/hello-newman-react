2f39197b01e8a36eee61980b9c3c311d1078e238
Jason <jarnewma@gmail.com>
2021-03-24 21:50:30 -0400

    more styling changes and cleanup

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,25 +1,23 @@
 import React from "react";
-import image from "../../assets/solid-silver.png";
+import image from "../../assets/hello-newman3.webp";
 
 export const Home = () => {
   return (
     <main>
-      <img src={image} alt="Nebula" className="absolute w-full h-full" />
+      <img src={image} alt="Seinfeld" className="absolute w-full h-full" />
       <article className="container shadow-lg mx-auto">
         <header className="relative">
-          <div className="relative h-full w-full flex items-center justify-center p-8">
-            <div className="relative items-center justify-center bg-opacity-75 rounded p-12">
-              <h1 className="text-center text-4xl text-blue-100 font-bold cursive leading-none lg:leading-snug home-name">
-                {" "}
-                Hi, I'm Jason{" "}
-              </h1>
-              <div className="flex justify-center text-2xl text-blue-100">
-                I'm an entrepreneur/software engineer based in Indianapolis. I
-                consider myself a generalist with the ability to shift focus
-                towards whatever the business' needs may be, but I'm most
-                passionate about developing creative solutions to the problems
-                that need solving. Thanks for stopping by!
-              </div>
+          <div className="absolute inset-y-40 left-0 bg-opacity-75 rounded p-12">
+            <h1 className="text-center text-4xl text-blue-100 font-bold cursive leading-none lg:leading-snug home-name">
+              {" "}
+              Hi, I'm Jason{" "}
+            </h1>
+            <div className="relative items-center text-2xl text-blue-100">
+              I'm an entrepreneur/software engineer based in Indianapolis. I
+              consider myself a generalist with the ability to shift focus
+              towards whatever the business' needs may be, but I'm most
+              passionate about developing creative solutions to the problems
+              that need solving. And yes, my last name is actually Newman. Thanks for stopping by! 
             </div>
           </div>
         </header>
